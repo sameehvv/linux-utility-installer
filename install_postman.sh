aa65293ab2ff44d7fd74b6f366d2a0ee9c82fab5
Sameeh <77770369+sameehvv@users.noreply.github.com>
2023-05-14 23:19:21 +0530

    Delete install_postman.sh

--- install_postman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://dl.pstmn.io/download/latest/linux_64 -O postman-linux-x64.tar.gz
-tar -xzf postman-linux-x64.tar.gz
-sudo mv Postman /opt/
-sudo ln -s /opt/Postman/Postman /usr/local/bin/postman
-rm postman-linux-x64.tar.gz
